707570d38cab8c823be3d3edefed4d1fb8a56cf1
PaoloReyes <paolo.alfonso.reyes@gmail.com>
2025-02-18 17:43:04 -0600

    fix(Serial): Baudrate fix for Serial 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,23 @@
 #include <differential_car.h>
 #include <constants.h>
 #include <utils.h>
+#include<SoftwareSerial.h>
 
 DifferentialCar car (LEFT_MOTOR_IN1, LEFT_MOTOR_IN2, LEFT_MOTOR_EN, 
                       LEFT_MOTOR_ENC_A, LEFT_MOTOR_ENC_B, LEFT_MOTOR_KP, LEFT_MOTOR_KI, 
                       RIGHT_MOTOR_IN1, RIGHT_MOTOR_IN2, RIGHT_MOTOR_EN, 
                       RIGHT_MOTOR_ENC_A, RIGHT_MOTOR_ENC_B, RIGHT_MOTOR_KP, RIGHT_MOTOR_KI);
 
-RS485 rs485(&Serial, READ_WRITE_ENABLER);
+RS485 rs485(&Serial2, READ_WRITE_ENABLER);
 
 void setup(void) {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial2.begin(9600);
   car.init();
   rs485.write("OP 1");
 }
 
 void loop(void) {
   Serial.println(rs485.read());
+  delay(1000);
 }
